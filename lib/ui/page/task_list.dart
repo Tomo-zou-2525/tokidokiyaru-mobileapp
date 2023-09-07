b4c021da1568a89ecd54bae9a57d7810c539acb0
Yuto Ueki <ueki.yuto.bz@gmail.com>
2023-09-07 17:59:36 +0900

    テスト追加

--- lib/ui/page/task_list.dart
+++ lib/ui/page/task_list.dart
@@ -1,65 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tokidoki_mobile/usecase/state/task_list.dart';
+import 'package:tokidoki_mobile/domain/entity/task.dart';
+import 'package:tokidoki_mobile/ui/component/loader.dart';
 import 'package:tokidoki_mobile/ui/page/add_task.dart';
 import 'package:tokidoki_mobile/ui/page/edit_task.dart';
-import 'package:tokidoki_mobile/ui/component/loader.dart';
+import 'package:tokidoki_mobile/usecase/state/task_list.dart';
 
 class TaskListPage extends ConsumerWidget {
   const TaskListPage({Key? key}) : super(key: key);
+
+  Widget createTaskList(List<Task> taskList, WidgetRef ref) {
+    return ReorderableListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: taskList.length,
+      itemBuilder: (BuildContext context, int index) {
+        final task = taskList[index];
+        return Padding(
+          key: ValueKey(task.id),
+          padding: const EdgeInsets.all(1.0),
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(
+              border:
+                  Border.all(color: Theme.of(context).colorScheme.secondary),
+              color: Theme.of(context).colorScheme.surface,
+            ),
+            child: ListTile(
+              contentPadding: const EdgeInsets.all(8.0),
+              title: Text(
+                '${task.name} ${task.lastRunDate}',
+                style: const TextStyle(fontSize: 20),
+              ),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EditTaskPage(task: task),
+                    ),
+                  );
+                },
+                child: const Icon(Icons.punch_clock, size: 40),
+              ),
+            ),
+          ),
+        );
+      },
+      onReorder: (int oldIndex, int newIndex) {
+        if (oldIndex < newIndex) {
+          newIndex -= 1;
+        }
+        final task = taskList.removeAt(oldIndex);
+        taskList.insert(newIndex, task);
+        ref.read(taskListNotifierProvider.notifier).updateOrder(taskList);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final taskListFuture = ref.watch(taskListNotifierProvider);
     final mainWidget = taskListFuture.when(
-      loading: () => createProgressIndicator(context),
-      error: (e, s) => const Text('タスク一覧の取得に失敗しました。'),
-      data: (taskList) => ReorderableListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: taskList.length,
-        itemBuilder: (BuildContext context, int index) {
-          final task = taskList[index];
-          return Padding(
-            key: ValueKey(task.id),
-            padding: const EdgeInsets.all(1.0),
-            child: Container(
-              height: 80,
-              decoration: BoxDecoration(
-                border:
-                    Border.all(color: Theme.of(context).colorScheme.secondary),
-                color: Theme.of(context).colorScheme.surface,
-              ),
-              child: ListTile(
-                contentPadding: const EdgeInsets.all(8.0),
-                title: Text(
-                  '${task.name} ${task.lastRunDate}',
-                  style: const TextStyle(fontSize: 20),
-                ),
-                trailing: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EditTaskPage(task: task),
-                      ),
-                    );
-                  },
-                  child: const Icon(Icons.punch_clock, size: 40),
-                ),
-              ),
+        loading: () => createProgressIndicator(context),
+        error: (e, s) => const Text(
+              'タスク一覧の取得に失敗しました。',
+              style: TextStyle(fontSize: 20),
             ),
-          );
-        },
-        onReorder: (int oldIndex, int newIndex) {
-          if (oldIndex < newIndex) {
-            newIndex -= 1;
-          }
-          final task = taskList.removeAt(oldIndex);
-          taskList.insert(newIndex, task);
-          ref.read(taskListNotifierProvider.notifier).updateOrder(taskList);
-        },
-      ),
-    );
+        data: (taskList) => taskList.isEmpty
+            ? const Text(
+                'タスクを追加して下さい。',
+                style: TextStyle(fontSize: 20),
+              )
+            : createTaskList(taskList, ref));
 
     return Scaffold(
       appBar: AppBar(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,23 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:tokidoki_mobile/domain/entity/task.dart';
+import 'package:tokidoki_mobile/domain/repository/repository.dart';
 import 'package:tokidoki_mobile/main.dart';
 
+import './fake_api.dart';
+import './utility.dart';
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+  testWidgets('タスク一覧が空の時にメッセージが表示されること', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      ProviderScope(overrides: [
+        repositoryProvider.overrideWithValue(FakeAPI(taskList: []))
+      ], child: const App()),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    expect(find.text('タスクを追加して下さい。'), findsOneWidget);
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('タスク一覧が表示されること', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      ProviderScope(overrides: [
+        repositoryProvider.overrideWithValue(FakeAPI(taskList: [
+          Task(
+            id: 1,
+            name: 'タスク1',
+            order: 1,
+            runAtList: [DateTime.parse('2023-03-20 10:05:30.000')],
+          ),
+          Task(
+            id: 3,
+            name: 'タスク2',
+            order: 2,
+            runAtList: [DateTime.parse('2023-05-01 01:04:11.000')],
+          ),
+          Task(
+            id: 2,
+            name: 'タスク3',
+            order: 3,
+            runAtList: [DateTime.parse('2023-04-29 18:53:55.000')],
+          ),
+        ]))
+      ], child: const App()),
+    );
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(ListTile), findsNWidgets(3));
+    expect(findPartialText('タスク1'), findsOneWidget);
+    expect(findPartialText('タスク2'), findsOneWidget);
+    expect(findPartialText('タスク3'), findsOneWidget);
   });
 }
