9a9eca6e33121d98981adccd37ab5bfc91a5fe58
RyoSakon001 <ryo.development0320@gmail.com>
2024-02-18 19:47:34 +0900

    FloationgButtonの位置設定を切り出し

--- lib/ui/style/customize_floating_location.dart
+++ lib/ui/style/customize_floating_location.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class CustomizeFloatingLocation extends FloatingActionButtonLocation {
   @override
-  Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) =>
-      const Offset(0, -50);
+  Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
+    final Offset offset =
+        FloatingActionButtonLocation.endFloat.getOffset(scaffoldGeometry);
+    return Offset(offset.dx + 0, offset.dy + (-50));
+  }
 }
