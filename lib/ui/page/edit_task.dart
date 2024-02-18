05fa3fd56cc61b80d57603a3121abaa408f7c4f8
RyoSakon001 <ryo.development0320@gmail.com>
2024-02-19 00:43:57 +0900

    EditTaskPageにもバナー広告を追加

--- lib/ui/page/edit_task.dart
+++ lib/ui/page/edit_task.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tokidoki_mobile/domain/entity/task.dart';
+import 'package:tokidoki_mobile/ui/component/admob/bottom_ad_banner.dart';
+import 'package:tokidoki_mobile/ui/component/common/base_drawer.dart';
 import 'package:tokidoki_mobile/ui/component/dialog/delete_done_at_confirmation_dialog.dart';
 import 'package:tokidoki_mobile/ui/component/dialog/delete_task_confirmation_dialog.dart';
 import 'package:tokidoki_mobile/ui/component/simple_app_bar.dart';
+import 'package:tokidoki_mobile/ui/style/customize_floating_location.dart';
 import 'package:tokidoki_mobile/usecase/state/task_list.dart';
 
 // TODO: doneAt削除後にタスクが更新されていないので修正する
@@ -34,12 +37,14 @@ class EditTaskPage extends HookConsumerWidget {
 
     return Scaffold(
       appBar: SimpleAppBar(title: watchedTask.name),
-      body: Center(
-        child: SizedBox(
-          child: Column(
-            children: [
-              isEditState.value
-                  ? Column(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  if (isEditState.value)
+                    Column(
                       children: [
                         const Text('タスク名を変更'),
                         TextField(
@@ -49,54 +54,54 @@ class EditTaskPage extends HookConsumerWidget {
                           ),
                         ),
                       ],
-                    )
-                  : Text(watchedTask.name),
-              const Text('実施履歴'),
-              Column(
-                children: watchedTask.dones
-                    .map((done) => ListTile(
-                          title: Text(done.doneDateAtString),
-                          trailing: isEditState.value
-                              ? ElevatedButton(
-                                  child: const Icon(Icons.delete),
-                                  onPressed: () {
-                                    showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return DeleteDoneAtConfirmationDialog(
-                                            done: done);
-                                      },
-                                    );
-                                  },
-                                )
-                              : null,
-                        ))
-                    .toList(),
-              ),
-              if (isEditState.value)
-                ElevatedButton(
-                    onPressed: () {
-                      showDialog<bool>(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return DeleteTaskConfirmationDialog(
-                              task: watchedTask);
-                        },
-                      ).then((result) {
-                        if (result == true) {
-                          Navigator.pop(context);
-                        }
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
                     ),
-                    child: const Text(
-                      'タスクを削除',
-                      style: TextStyle(color: Colors.white),
-                    )),
-            ],
-          ),
+                  const Text('実施履歴'),
+                  Column(
+                    children: watchedTask.dones
+                        .map((done) => ListTile(
+                              title: Text(done.doneDateAtString),
+                              trailing: isEditState.value
+                                  ? ElevatedButton(
+                                      child: const Icon(Icons.delete),
+                                      onPressed: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return DeleteDoneAtConfirmationDialog(
+                                                done: done);
+                                          },
+                                        );
+                                      },
+                                    )
+                                  : null,
+                            ))
+                        .toList(),
+                  ),
+                  if (isEditState.value)
+                    ElevatedButton(
+                      onPressed: () {
+                        showDialog<bool>(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return DeleteTaskConfirmationDialog(
+                                task: watchedTask);
+                          },
+                        ).then((result) {
+                          if (result == true) {
+                            Navigator.pop(context);
+                          }
+                        });
+                      },
+                      style:
+                          ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                      child: const Text('タスクを削除',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                ],
+              ),
+            ),
+            const BottomAdBanner(),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -120,6 +125,8 @@ class EditTaskPage extends HookConsumerWidget {
           isEditState.value ? Icons.check : Icons.edit,
         ),
       ),
+      floatingActionButtonLocation: SimpleFloatingLocation(),
+      endDrawer: const BaseDrawer(),
     );
   }
 }
