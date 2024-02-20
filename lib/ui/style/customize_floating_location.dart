6f10323ab956b8abb858374fcd8d8c11a3a096ad
RyoSakon001 <ryo.development0320@gmail.com>
2024-02-20 18:46:24 +0900

    50→60

--- lib/ui/style/customize_floating_location.dart
+++ lib/ui/style/customize_floating_location.dart
@@ -5,6 +5,6 @@ class SimpleFloatingLocation extends FloatingActionButtonLocation {
   Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
     final Offset offset =
         FloatingActionButtonLocation.endFloat.getOffset(scaffoldGeometry);
-    return Offset(offset.dx + 0, offset.dy + (-50));
+    return Offset(offset.dx + 0, offset.dy + (-60));
   }
 }
